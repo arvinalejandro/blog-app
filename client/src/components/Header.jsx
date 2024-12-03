1adb55679e09f317e0393efe539658494734b1fb
arvin <arvinalejandro@gmail.com>
2024-12-04 05:11:44 +0800

    fixed navbar logo and signup ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,52 +1,75 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { CgDarkMode } from 'react-icons/cg';
+import logo from '../assets/arvin_logo.png';
 
 export default function Header() {
   const path = useLocation().pathname;
+  const activeNav = 'text-gray-800 hover:text-gray-800';
+  const inActiveNav = 'text-gray-500 hover:text-gray-800';
   return (
-    <Navbar className='border-b-2'>
-      <Link
-        to='/'
-        className='self-center whitespace-nowrap text-sm:text-xl font-semibold dark:text-white'
-      >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 rounded-lg text-white'>
-          AA
+    <Navbar className='border-b-2 border-gray-400 bg-gray-300 fixed w-full top-0 left-0 z-50'>
+      <Navbar.Brand as={Link} href='/'>
+        <img
+          src={logo}
+          className='mr-3 h-6 w-9 sm:h-9 rounded-lg'
+          alt='Arvin Logo'
+        />
+        <span className='self-center whitespace-nowrap text-xl font-serif font-semibold dark:text-white'>
+          Archives
         </span>
-        Blog
-      </Link>
-      <form>
+      </Navbar.Brand>
+      <form className='lg:order-3'>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
+          className='hidden lg:inline '
         />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon />
+      <div className='flex gap-2 md:order-3'>
+        <Button
+          className='w-12 h-10 hidden sm:inline text-xl'
+          color='gray'
+          pill
+        >
+          <CgDarkMode />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
+          <Button
+            className='bg-gradient-to-r from-gray-800 to-gray-600'
+            outline
+          >
             Sign In
           </Button>
         </Link>
       </div>
       <Navbar.Toggle />
-      <Navbar.Collapse>
+      <Navbar.Collapse className='lg:order-1'>
         <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
+          <Link className={path === '/' ? activeNav : inActiveNav} to='/'>
+            Home
+          </Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='about'>About</Link>
+          <Link
+            className={path === '/about' ? activeNav : inActiveNav}
+            to='about'
+          >
+            About
+          </Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
+          <Link
+            className={path === '/projects' ? activeNav : inActiveNav}
+            to='/projects'
+          >
+            Projects
+          </Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,60 @@
+import { Link } from 'react-router-dom';
+import { Button, Label, TextInput } from 'flowbite-react';
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/*Left*/}
+        <div className='flex-1'>
+          <Link to='/' className='text-4xl font-bold dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-black via-gray-700 to-gray-500  rounded-lg text-white'>
+              AA
+            </span>
+          </Link>
+          <p className='text-sm mt-5'>
+            This blog site lets you easily create, publish, and archive articles
+            to share your ideas with the world.
+            <br /> <span className='font-medium'>To get started:</span> <br />
+            Register with your email and password or sign up using your Google
+            account. Begin sharing your stories today!
+          </p>
+        </div>
+        {/*Right*/}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username' />
+              <TextInput type='text' placeholder='Username' id='username' />
+            </div>
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='text'
+                placeholder='name@company.com'
+                id='email'
+              />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='text' placeholder='Password' id='password' />
+            </div>
+            <Button
+              className='bg-gradient-to-r from-gray-800 to-gray-600'
+              type='submit'
+            >
+              Sign Up
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span className='font-medium'>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-400'>
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
