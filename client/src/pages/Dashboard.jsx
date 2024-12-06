b93452e2e1d73027f5e20a8828f88578e1210fe5
arvin <arvinalejandro@gmail.com>
2024-12-06 09:30:30 +0800

    create sidebar and dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) setTab(tabFromUrl);
+    // console.log(tabFromUrl);
+  }, [location]);
+
+  return (
+    <div className='min-h-screen mt-16 flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/*Side Bar*/}
+        <DashSidebar />
+      </div>
+      {/**Profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
+  );
 }
