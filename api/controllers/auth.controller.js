4a9a077a2a702945ce22754806f69df24f8f4a15
arvin <arvinalejandro@gmail.com>
2024-12-08 03:39:16 +0800

    add admin user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
-
+//====
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -49,10 +49,13 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
 
     if (!validPassword) {
-      return next(errorHandler(400, 'mali password.'));
+      return next(errorHandler(400, 'invalid password.'));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     const { password: pass, ...rest } = validUser._doc;
     res
       .status(200)
@@ -64,14 +67,17 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
-
+//====
 export const google = async (req, res, next) => {
   const { name, email, googlePhotoUrl } = req.body;
 
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -93,7 +99,10 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
