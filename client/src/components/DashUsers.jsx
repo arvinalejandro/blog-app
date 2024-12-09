5471d8302827ef776e2141a1b22164fc53bae061
arvin <arvinalejandro@gmail.com>
2024-12-10 06:17:58 +0800

    add delete user functionality

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -6,7 +6,7 @@ import { DeleteModal } from './DeleteModal';
 import { RiDeleteBinLine } from 'react-icons/ri';
 import { FaCheck, FaTimes } from 'react-icons/fa';
 
-export default function DashPost() {
+export default function DashUsers() {
   const { currentUser } = useSelector((state) => state.user);
   const [users, setUsers] = useState([]);
   const [showMore, setShowMore] = useState(true);
@@ -35,17 +35,14 @@ export default function DashPost() {
   const handleDeleteUser = async () => {
     setShowModal(false);
     try {
-      const res = await fetch(
-        `/api/user/deleteuser/${userIdToDelete}/${currentUser._id}`,
-        {
-          method: 'DELETE',
-        }
-      );
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: 'DELETE',
+      });
       const data = await res.json();
       if (!res.ok) {
         console.log(data.message);
       } else {
-        setUsers((prev) => prev.filter((post) => post._id !== userIdToDelete));
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
       }
     } catch (error) {
       console.log(error);
@@ -140,7 +137,7 @@ export default function DashPost() {
           )}
         </>
       ) : (
-        <p>No posts to display</p>
+        <p>No Users to display</p>
       )}
       <DeleteModal
         modalMessage='Are you sure you want to this user?'
